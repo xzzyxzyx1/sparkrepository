e6d84650c5a64d5cd8e4b4f7854e22cbc63dbffc
xzzyxzyx1 <xzzyxzyx1@gmail.com>
2019-03-10 23:45:56 +0530

    bb

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class HelloWorld {
 	//abcde
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Hello World")
     }
 
     public static int getSum(int x, int y) {
