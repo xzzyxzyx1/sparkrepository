f3e648b046d40aea724d37ff918d71ef4c0c8172
xzzyxzyx1 <xzzyxzyx1@gmail.com>
2019-03-10 15:06:18 +0530

    bb

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -5,5 +5,6 @@ import static spark.Spark.*;
 public class HelloWorld {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+        System.out.println("abcd");
     }
 }
